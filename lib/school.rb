504839f89282812c3045f1ba9e68e3becc15c208
Jacob W Jones <jacobwadejones76@gmail.com>
2021-02-25 18:49:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,13 @@ class School
     @roster = {}
   end
   
+  def add_student(student_name, student_grade)
+    if @roster[student_grade] == nil
+      @roster[student_grade] = [student_name]
+    else
+      @roster[student_grade] << student_name
+    end
+  
   
   
 end
